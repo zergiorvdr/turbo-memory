a636e70b711e661aec95ed515331f17ab04b66d7
zergiorvdr <imamasyari330@gmail.com>
2023-09-18 16:07:32 +0700

    pmbaruanhg

--- frontend/src/Database/Database.js
+++ frontend/src/Database/Database.js
@@ -1,6 +1,7 @@
 import image1 from '../images/1.jpg';
 import image2 from '../images/2.jpg';
 import image3 from '../images/3.jpg';
+import image4 from '../images/4.jpg';
 
 export function getData(){
   return [
