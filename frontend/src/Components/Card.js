535e1cb1268bbfeea613c006d6cd6785fd457e05
zergiorvdr <imamasyari330@gmail.com>
2023-09-20 00:35:00 +0700

    frontendUpdates

--- frontend/src/Components/Card.js
+++ frontend/src/Components/Card.js
@@ -1,22 +1,28 @@
 import React, { useState } from 'react';
 import '../Card.css';
+
 function Card ({talent}) {
+  const [isActive, setIsActive] = useState(false);
   const [count, setCount] = useState(0);
-  const { title, image, price, id } = talent;
+  const { title, image, id } = talent;
+
+  const toggleHover = () => {
+    setIsActive(!isActive);
+  };
+
   return (
-    <div className="card">
-      <span
-      className= {`${count !== 0 ? "card__badge" : "card__badge--hidden"}`}>
-       {count}
+    <div className={`card ${isActive ? 'active' : ''}`} onClick={toggleHover}>
+      <span className={`${count !== 0 ? "card__badge" : "card__badge--hidden"}`}>
+        {count}
       </span>
-       <div className="card_video">
-         <img className="image"src={image} alt="image" />
-         <h4 className="card_title"> 
-           {title} <span className="price"> • {price} </span>
-         </h4>
-       </div>
+      <div className="card_video">
+        <img className="image"src={image} alt="image" />
+        <h4 className="card_title"> 
+          {title}
+        </h4>
+      </div>
     </div>
-     ) 
+  ) 
 }
 
 export default Card;
